4b338d21c5251bdf3efa3f0dbe27ede3a664992e
Marcel Riera <mrier011@fiu.edu>
2019-05-19 11:23:04 -0400

    Fixed wrong error display and user not found

--- modules/loadUserAPI.js
+++ modules/loadUserAPI.js
@@ -6,7 +6,7 @@ const accessToken = process.env.accessToken;
 module.exports = async (profile) => {
 
     const query = `query {
-        user(login: ${profile}) {
+        user(login: "${profile}") {
           name: repositories(last: 100, isFork: false) {
             nodes {
               languages (last: 100) {
@@ -31,8 +31,8 @@ module.exports = async (profile) => {
     });
     const api = await response.json();
 
-    if (api.message) {
-        return {error: userData.message};
+    if (api.errors) {
+        return {error: api.errors[0].message};
     }
 
     let languageStats = {};
@@ -54,36 +54,3 @@ module.exports = async (profile) => {
     const statsArray = Object.entries(languageStats).sort((A,B) => (B[1].value - A[1].value));
     return statsArray;
 }
-
-
-
-/*
-
-    const oauth = '?per_page=100&client_id=' + clientID + '&client_secret=' + clientSecret;
-    const URL = 'https://api.github.com/users/' + profile + '/repos' + oauth;   
-    const userData = await ( await fetch(URL) ).json();   
-    if (userData.message) {
-        return {error: userData.message};
-    }
-    
-    const rawLang = await Promise.all(userData.map(repo => fetch(repo.languages_url + oauth)));
-    const langData = await Promise.all(rawLang.map(raw => raw.json()));
-
-    let languageStats = {};
-    langData.forEach(repoLang => {
-        const keys = Object.keys(repoLang);
-        keys.forEach(language => {
-            if (!languageStats[language]) {
-                languageStats[language] = {
-                    value: 0,
-                    color: palette[language]
-                }
-            }
-            languageStats[language].value += repoLang[language];
-        })
-    });
-    return statsArray;
-}
-
-
-*/
