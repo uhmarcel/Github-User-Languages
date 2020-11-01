95cd3d0593324fc6fcf3802cf4ab5407dd7e200d
Marcel Riera <marcel_riera@ultimatesoftware.com>
2020-11-01 00:05:30 -0400

    Updates

---
+++ precache-manifest.f5b248c47fafcdeb6b43e0e252bffbe4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3c9285f719851cb1a6acfb73713088",
+    "url": "/Github-User-Languages/index.html"
+  },
+  {
+    "revision": "ded03551d11aa4059fce",
+    "url": "/Github-User-Languages/static/css/main.c7e0edec.chunk.css"
+  },
+  {
+    "revision": "1a4058c85d63674514e9",
+    "url": "/Github-User-Languages/static/js/2.5fd531f1.chunk.js"
+  },
+  {
+    "revision": "ded03551d11aa4059fce",
+    "url": "/Github-User-Languages/static/js/main.e4c409a3.chunk.js"
+  },
+  {
+    "revision": "1f44775a1d3ad77ff981",
+    "url": "/Github-User-Languages/static/js/runtime~main.dd83dc62.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.57223a524e18fe581a936214789468e8.js"
+  "/Github-User-Languages/precache-manifest.f5b248c47fafcdeb6b43e0e252bffbe4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Github-User-Languages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
