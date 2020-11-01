d6522545d763b2171f0c8169871ef9cd3ae358f3
Marcel Riera <marcel_riera@ultimatesoftware.com>
2020-11-01 00:47:53 -0400

    Updates

---
+++ precache-manifest.a18e2cab1551340ab9f5dc400d591631.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39819cd1d18da5c608f6a83b96dac500",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc4066e14c5ee4cdddfb",
+    "url": "/static/css/main.c7e0edec.chunk.css"
+  },
+  {
+    "revision": "1a4058c85d63674514e9",
+    "url": "/static/js/2.5fd531f1.chunk.js"
+  },
+  {
+    "revision": "dc4066e14c5ee4cdddfb",
+    "url": "/static/js/main.6c30d587.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
